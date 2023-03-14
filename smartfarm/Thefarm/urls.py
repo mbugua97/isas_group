9ee59b24e53172a6f0d266a6bc598415a9bc06f9
mbugua <jobugua97@gmail.com>
2023-03-14 19:30:35 +0300

    dupdated switching mechanism

--- smartfarm/Thefarm/urls.py
+++ smartfarm/Thefarm/urls.py
@@ -7,7 +7,12 @@ urlpatterns=[
     path('homepage/',views.homepage,name='hpage'),#this is the home page
     path('fogotpassword/',views.fogotpssd,name='fogotp'),#fogot password
     path('newuser/',views.newuser,name='newuser'),#new user
-    path('lotp/',views.lotp,name='lotp')#loginviaotp
+    path('lotp/',views.lotp,name='lotp'),#loginviaotp
+
+
+    path('switch-bulb/', views.Switch_bulb, name='sbulb'),
+    path('switch-fan/', views.Switch_fan, name='sfan'),
+    path('switch-pump/', views.Switch_pump, name='spump'),
 
 ]
 
--- smartfarm/Thefarm/views.py
+++ smartfarm/Thefarm/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 import cv2
 import threading
 from django.http import StreamingHttpResponse
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+
 
 #this is the login page view
 
@@ -30,13 +33,32 @@ def newuser(request):
     return render(request,'Thefarm/newuser.html')
 
 
-
+#this is the otp login
 def lotp(request):
     return render(request,'Thefarm/lotp.html')
 
 
 
 
+#switching the bulb
+@csrf_exempt
+def Switch_bulb(request):
+    success = True  # or False, depending on whether the script executed successfully or not
+
+    return JsonResponse({'success': success})
 
 
+#switching the fun
+@csrf_exempt
+def Switch_fan(request):
+    success = True  # or False, depending on whether the script executed successfully or not
+
+    return JsonResponse({'success': success})
+
+#switching the pump
+@csrf_exempt
+def Switch_pump(request):
+    success = True  # or False, depending on whether the script executed successfully or not
+
+    return JsonResponse({'success': success})
 
